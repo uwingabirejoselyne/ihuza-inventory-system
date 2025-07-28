c0a7e372007fd73df795abb6c94af460e2b8081d
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-07-28 10:35:37 +0200

    login user context

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-primary-900'>hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/contexts/LoginUserContext.jsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { createContext, useState } from "react"
+
+export const LoginUserContext = createContext()
+
+export const LoginUserProvider = ({ children }) => {
+  const [loginUser, setLoginUser] = useState({
+    username: "Admin",
+    email: "admin@ihuza.com",
+    role: "Admin",
+  })
+
+  const login = (userData) => {
+    setLoginUser(userData)
+  }
+
+  const logout = () => {
+    setLoginUser(null)
+  }
+
+  return (
+    <LoginUserContext.Provider
+      value={{
+        loginUser,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </LoginUserContext.Provider>
+  )
+}
